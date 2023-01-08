fe0a09481f7a5b035ea4bc05eca2f8d30b254c1a
Ibyaheem <ha585115@gmail.com>
2023-01-08 15:39:11 +0200

    Initial commit

---
+++ lib/layout/home_layout.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/modules/archived_tasks/archived_tasks.dart';
+import 'package:todo_app/modules/done_tasks/done_tasks.dart';
+import 'package:todo_app/modules/new_tasks/new_tasks.dart';
+
+class HomeLayout extends StatefulWidget {
+  const HomeLayout({Key? key}) : super(key: key);
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
+  int currentIndex = 0;
+
+  List<Widget> screens = [
+    NewTasks(),
+    DoneTasks(),
+    ArchivedTasks(),
+  ];
+
+  List<String> titles = [
+    'New Tasks',
+    'Done Tasks',
+    'Archived Tasks',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '${titles[currentIndex]}',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.menu,
+            ),
+            label: 'Tasks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.check_circle_outline,
+            ),
+            label: 'Done',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.archive_outlined,
+            ),
+            label: 'Archived',
+          ),
+        ],
+      ),
+      body: screens[currentIndex],
+    );
+  }
+}
